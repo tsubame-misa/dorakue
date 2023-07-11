b4f423ced7676cacf45cd206928352655f691f84
Misato Watanabe <watamisa080513@gmail.com>
2023-07-11 23:33:39 +0900

    edge_length_variance

--- common/aestheticsMeasures.py
+++ common/aestheticsMeasures.py
@@ -1,3 +1,6 @@
+import calcDrawInfo
+
+
 def calc_mean(array):
     return sum(array)/len(array)
 
@@ -18,3 +21,15 @@ def calc_evaluation_values(delta, dist_score):
 
     return {"delta": {"mean": delta_mean, "sd": delta_sd, "sum": delta_sum},
             "dist": {"mean": dist_mean, "sd": dist_sd, "sum": dist_sum}}
+
+
+def calc_edge_length_variance(pos, graph, node2num):
+    l = []
+    for x_node, y_node in graph.edges:
+        u = node2num[x_node]
+        v = node2num[y_node]
+        l.append(calcDrawInfo.dist(pos, u, v))
+    l_mean = calc_mean(l)
+    variance = sum([((l_mean/l_mean) - (l[i]/l_mean))
+                   ** 2 for i in range(l)])/len(l)
+    return variance
