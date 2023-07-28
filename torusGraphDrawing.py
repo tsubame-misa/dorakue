3072df286b2c5d6c855370e7d9975f8ce7f5e25b
Misato Watanabe <watamisa080513@gmail.com>
2023-07-28 10:35:40 +0900

    torus描画

--- torusGraphDrawing.py
+++ torusGraphDrawing.py
@@ -21,22 +21,70 @@ def draw_node(pos, ax):
         ax.add_patch(C)
 
 
-def draw_edge(graph, node2num,  pos, _len, ax):
+def draw_edge(graph, node2num,  pos, l,  _len, ax):
     edge_lines = []
+    wrap_lines = []
+    wrap_lines2 = []
 
     for i, j in graph.edges:
         idx_i = node2num[str(i)]
         idx_j = node2num[str(j)]
 
-        line = [(pos[idx_i][0], pos[idx_i][1]), (pos[idx_j][0], pos[idx_j][1])]
-        edge_lines.append(line)
+        best_pos, is_wrap = select_node(
+            pos, idx_i, idx_j, _len, l[idx_i][idx_j])
+
+        if is_wrap:
+            line = [(pos[idx_i][0], pos[idx_i][1]),
+                    (best_pos[0], best_pos[1])]
+            wrap_lines.append(line)
+
+            best_pos, is_wrap = select_node(
+                pos,  idx_j, idx_i, _len, l[idx_i][idx_j])
+            line = [(pos[idx_j][0], pos[idx_j][1]),
+                    (best_pos[0], best_pos[1])]
+            wrap_lines2.append(line)
+        else:
+            line = [(pos[idx_i][0], pos[idx_i][1]),
+                    (pos[idx_j][0], pos[idx_j][1])]
+            edge_lines.append(line)
 
     line_collection = collections.LineCollection(
         edge_lines, color=("green",), linewidths=(0.5,))
     ax.add_collection(line_collection)
 
+    line_collection = collections.LineCollection(
+        wrap_lines, color=("red",), linewidths=(0.5,))
+    ax.add_collection(line_collection)
 
-def graph_drawing(graph, pos, _len):
+    line_collection = collections.LineCollection(
+        wrap_lines2, color=("orange",), linewidths=(0.5,))
+    ax.add_collection(line_collection)
+
+
+def select_node(pos, u, v, _len, ideal_dist):
+    # uから見た
+    x_list = [pos[v][0]-_len, pos[v][0], pos[v][0]+_len]
+    y_list = [pos[v][1]-_len, pos[v][1], pos[v][1]+_len]
+
+    best_pos = [pos[v][0], pos[v][1]]
+    _dist = float("inf")
+
+    for x in x_list:
+        for y in y_list:
+            ax = pos[u][0] - x
+            ay = pos[u][1] - y
+            adist = (ax ** 2 + ay ** 2) ** 0.5
+            if abs(_dist-ideal_dist) > abs(adist-ideal_dist):
+                best_pos[0] = x
+                best_pos[1] = y
+                _dist = adist
+
+    is_wrap = not(best_pos[0] == pos[v][0] and best_pos[1] == pos[v][1])
+
+    return best_pos, is_wrap
+
+
+def graph_drawing(data, graph, _len):
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(111)
     ax.set_xlim(-_len, _len*2)
@@ -49,8 +97,9 @@ def graph_drawing(graph, pos, _len):
         cell_lines, color=("black",), linewidths=(0.5,))
     ax.add_collection(line_collection)
 
-    draw_node(pos, ax)
-    draw_edge(graph, data["node2num"], pos, _len, ax)
+    pos9 = create_pos9(data["pos"], _len)
+    draw_node(pos9, ax)
+    draw_edge(graph, data["node2num"], data["pos"], data["l"], _len, ax)
 
     plt.savefig("./sample.png")
     plt.clf()
@@ -64,7 +113,4 @@ with open("./result_sgd_0725_all_log/log/desargues-20230724233845.json") as f:
         json.load(open("./graph/desargues.json")))
     _len = 500
 
-    # pos9 = create_pos9(data["pos"], _len)
-    pos9 = data["pos"]
-
-    graph_drawing(graph, pos9, _len)
+    graph_drawing(data, graph, _len)
