8b60de4a70397e6f15c82601629889bd71d33bd1
Misato Watanabe <watamisa080513@gmail.com>
2024-06-21 16:38:10 +0900

    undo

--- algorithm/SGDBase/egraphTorusSGD.py
+++ algorithm/SGDBase/egraphTorusSGD.py
@@ -2,17 +2,19 @@ import os
 import networkx as nx
 import egraph as eg
 import matplotlib.pyplot as plt
-from common  import egraphCalcDrawInfo
+from common import egraphCalcDrawInfo
 import networkx as nx
 import egraph as eg
 import matplotlib.pyplot as plt
+
 
 def centered_graph(pos, graph):
     min_edge_len = float("inf")
     for p in pos:
         diff_x, diff_y, _pos = egraphCalcDrawInfo.shift_center(pos, p, 1, 1)
         max_edge_len = max(
-            egraphCalcDrawInfo.dist_around(_pos, u, v) for u, v in graph.edges)
+            egraphCalcDrawInfo.dist_around(_pos, u, v) for u, v in graph.edges
+        )
         if min_edge_len > max_edge_len:
             min_edge_len = max_edge_len
             center_idx = p
@@ -22,8 +24,16 @@ def centered_graph(pos, graph):
     return fin_pos
 
 
-
-def torus_sgd(original_graph, name, dir, multiple_num=1.0, random_idx=0, time="xxxx", is_chen=False, my_drawing=None):
+def torus_sgd(
+    original_graph,
+    name,
+    dir,
+    multiple_num=1.0,
+    random_idx=0,
+    time="xxxx",
+    is_chen=False,
+    my_drawing=None,
+):
     graph = eg.Graph()
     indices = {}
     for u in original_graph.nodes:
@@ -33,7 +43,7 @@ def torus_sgd(original_graph, name, dir, multiple_num=1.0, random_idx=0, time="x
 
     diameter = nx.diameter(original_graph)
     if is_chen:
-        multiple_num = (max(diameter, 2) + 1)/diameter
+        multiple_num = (max(diameter, 2) + 1) / diameter
 
     size = nx.diameter(original_graph) * multiple_num
     d = eg.all_sources_bfs(
@@ -51,40 +61,52 @@ def torus_sgd(original_graph, name, dir, multiple_num=1.0, random_idx=0, time="x
     def step(eta):
         sgd.shuffle(rng)
         sgd.apply(drawing, eta)
+
     scheduler.run(step)
 
-    pos = {u: (drawing.x(i) * size, drawing.y(i) * size)
-           for u, i in indices.items()}
+    pos = {u: (drawing.x(i) * size, drawing.y(i) * size) for u, i in indices.items()}
     nx_edge_graph = nx.Graph()
     edge_pos = {}
     for e in graph.edge_indices():
         u, v = graph.edge_endpoints(e)
         segments = drawing.edge_segments(u, v)
         for i, ((x1, y1), (x2, y2)) in enumerate(segments):
-            eu = f'{u}:{v}:{i}:0'
-            ev = f'{u}:{v}:{i}:1'
+            eu = f"{u}:{v}:{i}:0"
+            ev = f"{u}:{v}:{i}:1"
             nx_edge_graph.add_edge(eu, ev)
             edge_pos[eu] = (x1 * size, y1 * size)
             edge_pos[ev] = (x2 * size, y2 * size)
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.set_xlim(0, size)
     ax.set_ylim(0, size)
-    nx.draw_networkx_nodes(original_graph, pos, ax=ax, node_color="#6f6f6fcf", node_size=100)
+    nx.draw_networkx_nodes(
+        original_graph, pos, ax=ax, node_color="#6f6f6fcf", node_size=50
+    )
     nx.draw_networkx_edges(nx_edge_graph, edge_pos, ax=ax)
 
     if not os.path.isdir(dir):
         os.mkdir(dir)
-    img_path ='./'+dir+'/torusSGD_wrap/' + \
-        str(name) + '-' + str(multiple_num) + '-' + time + '.png'
+    img_path = (
+        "./"
+        + dir
+        + "/torusSGD_wrap/"
+        + str(name)
+        + "-"
+        + str(multiple_num)
+        + "-"
+        + time
+        + ".png"
+    )
     plt.savefig(img_path)
 
-
-    
     ec = eg.crossing_edges(graph, drawing)
-    log = {"multiple_num":multiple_num, 
-           "stress":eg.stress(drawing, d) , 
-           "edge_crossings":eg.crossing_number_with_crossing_edges(ec),
-           "minimum_angle": eg.crossing_angle_with_crossing_edges(ec), 
-           "node_resolution":eg.node_resolution(drawing), 
-           "pos":pos}
+    log = {
+        "multiple_num": multiple_num,
+        "stress": eg.stress(drawing, d),
+        "edge_length_vaiance": eg.ideal_edge_lengths(graph, drawing, d),
+        "edge_crossings": eg.crossing_number_with_crossing_edges(ec),
+        "minimum_angle": eg.crossing_angle_with_crossing_edges(ec),
+        "node_resolution": eg.node_resolution(drawing),
+        "pos": pos,
+    }
     return log
