7a7ad1dc07c704eac448ab2c2747e37c81b08c59
Misato Watanabe <watamisa080513@gmail.com>
2024-07-02 15:23:29 +0900

    apply weighting

--- journal/goldenSectionSearchTime.py
+++ journal/goldenSectionSearchTime.py
@@ -1,7 +1,9 @@
+import argparse
 import glob
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent)+"/")
+
+sys.path.append(str(Path(__file__).parent.parent) + "/")
 
 from networkx.readwrite import json_graph
 import json
@@ -14,43 +16,62 @@ from algorithm.SGDBase.egraphCellOptimazation import cell_optimazation
 
 """
 黄金分割探索の実行時間を測る
+files = ./graphSet/networkx
+files = ./graphSet/doughNetGraph/default
+files = ./graphSet/randomPartitionNetwork 
+files = ./graphSet/suiteSparse
 """
 
-def main():
-    files = glob.glob("./graphSet/networkx/*")
-    files = glob.glob("./graphSet/doughNetGraph/default/*")
-    files = glob.glob("./graphSet/randomPartitionNetwork /*")
-    files = glob.glob("./graphSet/suiteSparse/*")
 
+def main():
+    parser = argparse.ArgumentParser()  # parserを定義
+
+    # 受け取る引数を追加する
+    parser.add_argument("graph_file")  # 必須の引数を追加
+    parser.add_argument("log_file_name")
+    parser.add_argument("--weigthing", default=False)
+    parser.add_argument("--loop", default=20)
+
+    args = parser.parse_args()  # 引数を解析
+
+    files = glob.glob(args.graph_file + "/*")
+
+    golden_section_search(files, args.log_file_name, args.weigthing, int(args.loop))
+
+
+def golden_section_search(files, log_file_name, weigthing, loop):
     graphs = []
     for filepath in files:
         graph = json_graph.node_link_graph(json.load(open(filepath)))
-        file_name = re.split('[/]', filepath)[-1][:-5]
+        file_name = re.split("[/]", filepath)[-1][:-5]
         obj = {"name": file_name, "graph": graph}
         graphs.append(obj)
     sorted_graphs = sorted(graphs, key=lambda x: len(x["graph"].nodes))
 
-    log_file_name = "test_time_gss_sparse"
     setup.set_dir_name(log_file_name)
     log.create_log_folder()
 
     for g in sorted_graphs:
-        print(g["name"], len(g["graph"].nodes), len(g["graph"].edges),"-----------------")
+        print(
+            g["name"], len(g["graph"].nodes), len(g["graph"].edges), "-----------------"
+        )
         data = []
-        for i in range(20):
+        for i in range(loop):
             print(g["name"], i)
             _time = setup.get_time()
-            index_time = str(i) + str(_time)   
-            start = time.perf_counter() #計測開始    
-            _log = cell_optimazation(g["graph"], g["name"], log_file_name, i, index_time)
-            end = time.perf_counter() #計測終了
+            index_time = str(i) + str(_time)
+            start = time.perf_counter()  # 計測開始
+            _log = cell_optimazation(
+                g["graph"], g["name"], log_file_name, i, index_time, weigthing
+            )
+            end = time.perf_counter()  # 計測終了
             _log["id"] = index_time
-            _log["time"] = end-start
+            _log["time"] = end - start
             data.append(_log)
-        log.create_log(data, g["name"]+"-all")
-        
+        log.create_log(data, g["name"] + "-all")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 
 import time
